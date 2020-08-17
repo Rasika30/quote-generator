342a204bd82f2b2c5c5ca198d9c41cc59da17622
Rasika30 <rasigr8@gmail.com>
2020-08-17 21:00:51 +0530

    Added styling and functionality

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('testing');
+const quoteContainer = document.getElementById("quote-container")
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote")
+const loader = document.getElementById("loader");
+
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if(!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get quote from API
+async function getQuote() {
+    showLoadingSpinner();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        //If Author is blank, add 'Unknown'
+        authorText.innerText = '- ';
+        if(data.quoteAuthor === ''){
+            authorText.innerText = authorText.innerText+'Unknown';
+        } else{
+            authorText.innerText = authorText.innerText+data.quoteAuthor;
+        }
+        //Reducing font size for long quotes
+        if(data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+            authorText.classList.add('long-quote-author');
+        } else {
+            quoteText.classList.remove('long-quote');
+            authorText.classList.remove('long-quote-author');
+        }
+        quoteText.innerText = data.quoteText;
+        removeLoadingSpinner();
+    } catch(error) {
+        getQuote();
+    }
+}
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On Load 
+getQuote();
